f8eb74e353674a7295c51ca54181dafd9a2c9063
Albert Pla <alplsan@epsa.upv.es>
2019-10-28 12:21:51 +0100

    First Version Controller & Model

---
+++ src/main/kotlin/com/creditas/creditas/models/Movie.kt
@@ -0,0 +1,10 @@
+package com.creditas.creditas.models
+
+data class Movie (
+    val id:Int,
+    val titulo: String,
+    val director: String,
+    val enCines: Boolean,
+    val precioEntrada: Float
+
+)
