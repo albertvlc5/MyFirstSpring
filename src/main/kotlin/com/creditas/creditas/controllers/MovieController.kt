5dd54d943a94a7d40f765a09a1e481a561dc8048
Albert Pla <alplsan@epsa.upv.es>
2019-10-28 12:30:41 +0100

    API REST

--- src/main/kotlin/com/creditas/creditas/controllers/MovieController.kt
+++ src/main/kotlin/com/creditas/creditas/controllers/MovieController.kt
@@ -1,14 +1,19 @@
 package com.creditas.creditas.controllers
 
+import com.creditas.creditas.models.Movie
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MovieController {
 
     @GetMapping("/movies")
-    fun getAllMovies(){
+    fun getAllMovies(
+            @RequestParam(value = "name", defaultValue = "All") name: String) =
 
-    }
+        Movie(1,"SoyLeyenda","Albert",true,40.00F)
+
+
 
 }
